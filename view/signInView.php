3748b23cacb6996bd25fa0e4c176e01ab24258ff
florian bertin <florian.bertin1999@gmail.com>
2022-01-15 18:01:45 +0100

    first commit

---
+++ view/signInView.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Nicolas Devlaeminck, Loic Maurer, Florian Bertin, Lilian Laureau">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>Sign in : Ticket Manager</title>
+    <script type="text/javascript" src="signInScript.js"></script>
+    <link rel="icon" href="public/images/logo.png" />
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.css" rel="stylesheet"> <!-- a delete -->
+    <link href="public\css\bootstrap.css" rel="stylesheet">
+    <link href="public\css\dashboard.css" rel="stylesheet">
+
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="public\css\signin.css" rel="stylesheet"> 
+
+  </head>
+
+
+  <body class="text-center">
+
+  <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="#">
+          <img src="public/images/logo.png" alt="logo" style="width:60px;">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Products</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">About us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+   <!-- /.navbar -->
+
+
+    <!-- Header -->
+    <header id="head">
+      <div class="container">
+        <div class="row" id="HeaderModif">
+          <h1 class="lead">Come Experience BeyondFog</h1>
+          <!-- <p class="tagline">PROGRESSUS: free business bootstrap template by <a href="http://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a></p> !-->
+        </div>
+      </div>
+    </header>
+    <!-- /Header -->
+
+
+
+
+
+    <!-- Highlights - jumbotron -->
+    <div class="jumbotron top-space">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 highlight">
+              <img src="public/images/allo1.png" class="rounded" alt="Cinque Terre">
+              <div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Bootstrap-powered</h4></div>
+          </div>
+          <div class="col-md-4 col-sm-6 highlight">
+              <img src="public/images/allo2.png" class="rounded" alt="Cinque Terre">
+              <div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Bootstrap-powered</h4></div>
+          </div>
+          <div class="col-md-4 col-sm-6 highlight">
+                <img src="public/images/allo3.png" class="rounded" alt="Cinque Terre">
+                <div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Bootstrap-powered</h4></div>
+          </div>
+        </div> <!-- /row  -->
+        
+      </div>
+    </div>
+    <!-- /Highlights -->
+
+
+    <!-- container -->
+    <div class="container">
+
+      <h2 class="text-center top-space">Frequently Asked Questions</h2>
+      <br>
+
+      <div class="row">
+        <div class="col-sm-6">
+          <h3>Which code editor would you recommend?</h3>
+          <p>I'd highly recommend you <a href="http://www.sublimetext.com/">Sublime Text</a> - a free to try text editor which I'm using daily. Awesome tool!</p>
+        </div>
+        <div class="col-sm-6">
+          <h3>Nice header. Where do I find more images like that one?</h3>
+          <p>
+            Well, there are thousands of stock art galleries, but personally,
+            I prefer to use photos from these sites: <a href="http://unsplash.com">Unsplash.com</a>
+            and <a href="http://www.flickr.com/creativecommons/by-2.0/tags/">Flickr - Creative Commons</a></p>
+        </div>
+      </div> <!-- /row -->
+
+      <div class="row">
+        <div class="col-sm-6">
+          <h3>Can I use it to build a site for my client?</h3>
+          <p>
+            Yes, you can. You may use this template for any purpose, just don't forget about the <a href="http://creativecommons.org/licenses/by/3.0/">license</a>,
+            which says: "You must give appropriate credit", i.e. you must provide the name of the creator and a link to the original template in your work.
+          </p>
+        </div>
+        <div class="col-sm-6">
+          <h3>Can you customize this template for me?</h3>
+          <p>Yes, I can. Please drop me a line to sergey-at-pozhilov.com and describe your needs in details. Please note, my services are not cheap.</p>
+        </div>
+      </div> <!-- /row -->
+
+      <div class="jumbotron top-space">
+        <h4>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere magni ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia perspiciatis at voluptatibus. Libero, aliquid illum possimus numquam fuga.</h4>
+          <p class="text-right"><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+        </div>
+    </div>  <!-- /container -->
+
+  </body>
+</html>
